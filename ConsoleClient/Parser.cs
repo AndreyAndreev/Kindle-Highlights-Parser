1faa0f7fc96b1db55ce1d5fab3a13b090791ddd6
Andrey Andreev <AndreevAndrey@ya.ru>
2015-11-09 18:58:13 +0300

    First simple parser functional

---
+++ ConsoleClient/Parser.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using System.IO;
+
+namespace ConsoleClient
+{
+    public class Parser
+    {
+        private string _fileContent;
+
+        private List<Highlight> _highlights = new List<Highlight>();
+
+        private const string Separator = "==========";
+
+        public void OpenFile(string fileName)
+        {
+            using (StreamReader stream = File.OpenText(fileName))
+            {
+                int lineCounter = 0;
+                var highlight = new Highlight();
+
+                while (!stream.EndOfStream)
+                {
+                    string line = stream.ReadLine();
+                    lineCounter++;
+
+                    if (line != Separator)
+                    {
+                        switch (lineCounter)
+                        {
+                            case 1:
+                                highlight.Book = line;
+                                break;
+                            case 2:
+                                highlight.Info = line;
+                                break;
+                            case 4:
+                                highlight.Text = line;
+                                break;
+                        }
+                    }
+                    else
+                    {
+                        highlight.ParseInfo(highlight.Info);
+                        _highlights.Add(highlight);
+                        highlight = new Highlight();
+                        lineCounter = 0;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var parser = new Parser();
+            parser.OpenFile("D:\\temp\\highlights.txt");
+        }
+    }
+}
