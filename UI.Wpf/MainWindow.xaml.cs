2dd2ec153442e4af243bc8397a9d84ce9f839bc1
Andrey Andreev <AndreevAndrey@ya.ru>
2017-02-06 16:32:03 +0300

    Export clippings to html

--- UI.Wpf/MainWindow.xaml.cs
+++ UI.Wpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -52,6 +53,44 @@ namespace UI.Wpf
             var highlights = library.GetHighlightsForBookTitle(selectedTitle);
 
             Highlights.Text = string.Join("\n\n\n", highlights.Select(x => x.Text));
+        }
+
+        private void Export_Click(object sender, RoutedEventArgs e)
+        {
+            var sb = new StringBuilder();
+            sb.AppendLine("<!doctype html><html><body>");
+
+            int id = 0;
+
+            sb.AppendLine("<ul id=\"top\">");
+            foreach (var bookTitleWithCount in library.GetBookTitlesWithHighlightCounts())
+            {
+                sb.AppendLine($"<li>{bookTitleWithCount.Item1} – (<a href=\"#id_{id++}\">{bookTitleWithCount.Item2} highlights</a>)</li>");
+            }
+            sb.AppendLine("</ul>");
+
+            id = 0;
+            foreach (var bookTitleWithCount in library.GetBookTitlesWithHighlightCounts())
+            {
+                var title = $"{bookTitleWithCount.Item1} – ({bookTitleWithCount.Item2} highlights)";
+
+                sb.AppendLine($"<h1 id=\"id_{id++}\">{title}</h1>");
+
+                sb.AppendLine("<ul>");
+
+                foreach (var item in library.GetHighlightsForBookTitle(bookTitleWithCount.Item1))
+                {
+                    sb.AppendLine($"<li>{item.Text}</li>");
+                }
+
+                sb.AppendLine("</ul>");
+
+                sb.AppendLine("<a href=\"#top\">top</a>");
+            }
+
+            sb.AppendLine("</body></html>");
+
+            File.WriteAllText("clippings.html", sb.ToString());
         }
     }
 }
